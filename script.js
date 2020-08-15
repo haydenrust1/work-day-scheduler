224d0768c84aea8a3f25a24cb3a146d51bc6a1e1
Haydenrust1 <haydenrust1@gmail.com>
2020-08-15 15:18:55 -0700

    time blocks now represent past, present, future

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-$('document').ready(function () {});
+$(document).ready(function () {
+  //jquery selectors
+  var timeBlock = $('.time-block');
+  //   console.log(timeBlock);
+
+  //Display current day and date
+  $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+  //storing current whole hour
+  var currentHour = moment().hours();
+  console.log(currentHour);
+
+  //cycle through each time block to make either past, present, future
+  timeBlock.each(function () {
+    var timeBlockHour = $(this).data('time');
+    console.log(timeBlockHour);
+
+    if (timeBlockHour < currentHour) {
+      $(this).addClass('past');
+    } else if (timeBlockHour > currentHour) {
+      $(this).addClass('future');
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass('present');
+    }
+  });
+});
